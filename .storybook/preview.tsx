8713ab2db84dc1ec02fc64bb5ff3a0993d2cb94e
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-06-10 22:08:54 -0300

    fix: rename preview.js to preview.tsx

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
