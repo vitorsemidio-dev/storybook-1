022a37a48f5a81806ee4f98496a0fbf9e364c716
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-06-10 23:46:14 -0300

    ci: add setup configuration to deploy

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,5 +14,13 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
+  },
+  "staticDirs": ["../public"],
+  "viteFinal": (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/storybook-1/";
+    }
+
+    return config;
+  },
 }
